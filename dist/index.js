b85151e8baf4c7494ab3ed62a7cef8ec1ed323a9
NichikouGN <nichikougn@gmail.com>
2026-04-21 20:59:31 +0700

    Init

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const PORT = 4000;
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
